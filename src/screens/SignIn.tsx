493d5cc510b9eef5c9c71c95ef8ce78e6fa6bd75
Arthur Borges <borgesarthur.dev@gmail.com>
2024-09-07 16:49:21 -0300

    Button component

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@components/Button"
 import { Input } from "@components/Input"
 import { Logo } from "@components/Logo"
 import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
@@ -8,17 +9,19 @@ import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
 
 export const SignIn = () => {
     return (
-        <VStack flex={1} px="$12" pb="$16" bg="$gray600"  >
+        <VStack w="$full" flex={1} px="$12" pb="$16" bg="$gray600"  >
 
             <Center  my="$32">
                <Logo />
             </Center>
 
-            <Center w="$full" gap="$2">
+            <Center  gap="$2">
                 <Heading fontSize="$md" > Acesse sua conta </Heading>
 
                 <Input placeholder="CPF" autoCorrect={false} keyboardType="number-pad" />
                 <Input placeholder="Senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
+
+                <Button title="Entrar" />
             </Center>
 
         </VStack>
