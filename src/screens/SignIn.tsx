d03924323ea06bc2744dbddd56cb6c963a380dd9
Arthur Borges <borgesarthur.dev@gmail.com>
2024-09-09 22:13:56 -0300

    Fixing SignIn and SignUp style

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@components/Button"
 import { Input } from "@components/Input"
 import { Logo } from "@components/Logo"
-import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
+import { Center, Heading, HStack, ScrollView, Text, VStack } from "@gluestack-ui/themed"
 
 
 
@@ -9,29 +9,38 @@ import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
 
 export const SignIn = () => {
     return (
-        <VStack w="$full" flex={1} bg="$gray700"  >
-            <VStack w="$full" flex={1} px="$12" pb="$16" bg="$gray600" rounded="$3xl">
+        <ScrollView 
+            w="$full"
+            contentContainerStyle={{ flexGrow: 1}} 
+            showsVerticalScrollIndicator={false} 
+            automaticallyAdjustKeyboardInsets 
+            bounces={false}
+            $active-alignItems="center"
+        >
+            <VStack w="$full" flex={1} bg="$gray700"  >
+                <VStack w="$full" flex={1} px="$12" pb="$16" bg="$gray600" rounded="$3xl">
 
-                <Center  my="$32">
-                    <Logo />
+                    <Center  my="$33">
+                        <Logo />
+                    </Center>
+
+                    <Center  gap="$2">
+                        <Heading fontSize="$md" > Acesse sua conta </Heading>
+
+                        <Input placeholder="Username" autoCorrect={false} keyboardType="number-pad" />
+                        <Input placeholder="Senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
+
+                        <Button title="Entrar" my="$14" />
+                    </Center>
+
+                </VStack>
+                
+                <Center h="$56" flex={1} px="$12">
+                    <Heading fontSize="$sm" fontFamily="$heading" >Ainda não tem acesso? </Heading>
+
+                    <Button title="Crie uma conta" variant="clear" />
                 </Center>
-
-                <Center  gap="$2">
-                    <Heading fontSize="$md" > Acesse sua conta </Heading>
-
-                    <Input placeholder="CPF" autoCorrect={false} keyboardType="number-pad" />
-                    <Input placeholder="Senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
-
-                    <Button title="Entrar" my="$12" />
-                </Center>
-
             </VStack>
-            
-            <Center h="$56" px="$12">
-                <Heading fontSize="$sm" fontFamily="$heading" >Ainda não tem acesso? </Heading>
-
-                <Button title="Crie uma conta" variant="clear" />
-            </Center>
-        </VStack>
+        </ScrollView>
     )
 }
--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@components/Button"
 import { Input } from "@components/Input"
 import { Logo } from "@components/Logo"
-import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
+import { Center, Heading, HStack, Text, ScrollView, VStack } from "@gluestack-ui/themed"
 
 
 
@@ -9,35 +9,45 @@ import { Center, Heading, HStack, Text, VStack } from "@gluestack-ui/themed"
 
 export const SignUp = () => {
     return (
-        <VStack w="$full" flex={1} bg="$gray700"  >
-            <VStack w="$full" flex={1} px="$12" pb="$16" bg="$gray600" rounded="$3xl">
+        <ScrollView 
+            w="$full"
+            contentContainerStyle={{ flexGrow: 1}} 
+            showsVerticalScrollIndicator={false} 
+            automaticallyAdjustKeyboardInsets 
+            bounces={false}
+            $active-alignItems="center"
+        >
 
-                <Center  my="$24">
-                    <Logo />
+            <VStack w="$full" flex={1} bg="$gray700"  >
+                <VStack w="$full" flex={1} px="$12"  bg="$gray600" rounded="$3xl">
+
+                    <Center  my="$24">
+                        <Logo />
+                    </Center>
+
+                    <Center  gap="$2">
+                        <Heading fontSize="$md" > Crie sua conta </Heading>
+
+                        {/* <Input placeholder="CNPJ" autoCorrect={false} keyboardType="number-pad" />
+                        <Input placeholder="Razão Social" autoCorrect={false}  /> */}
+                        <Input placeholder="CPF" autoCorrect={false} keyboardType="number-pad" />
+                        <Input placeholder="Nome" autoCorrect={false}  />
+                        <Input placeholder="E-mail" autoCorrect={false} keyboardType="email-address" />
+                        <Input placeholder="Senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
+                        <Input placeholder="Confirmar senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
+
+
+                        <Button title="Criar e acessar" my="$12" />
+                    </Center>
+
+                </VStack>
+                
+                <Center h="$40" flex={1} px="$12">
+                    <Heading fontSize="$sm" fontFamily="$heading" >Já possui uma conta? </Heading>
+
+                    <Button title="Faça Login" variant="clear" />
                 </Center>
-
-                <Center  gap="$2">
-                    <Heading fontSize="$md" > Crie sua conta </Heading>
-
-                    <Input placeholder="CNPJ" autoCorrect={false} keyboardType="number-pad" />
-                    <Input placeholder="Razão Social" autoCorrect={false}  />
-                    <Input placeholder="Nome" autoCorrect={false}  />
-                    <Input placeholder="E-mail" autoCorrect={false} keyboardType="email-address" />
-                    <Input placeholder="CPF" autoCorrect={false} keyboardType="number-pad" />
-                    <Input placeholder="Senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
-                    <Input placeholder="Confirmar senha" autoCapitalize="none" autoCorrect={false} secureTextEntry />
-
-
-                    <Button title="Criar e acessar" my="$12" />
-                </Center>
-
             </VStack>
-            
-            <Center h="$56" px="$12">
-                <Heading fontSize="$sm" fontFamily="$heading" >Já possui uma conta? </Heading>
-
-                <Button title="Faça Login" variant="clear" />
-            </Center>
-        </VStack>
+        </ScrollView>
     )
 }
