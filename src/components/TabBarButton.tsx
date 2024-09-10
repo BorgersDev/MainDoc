b6264e7ffdf8c4cbd96c6aff01090eb03f217b66
Arthur Borges <borgesarthur.dev@gmail.com>
2024-09-10 16:10:03 -0300

    Tab bar icon animation working

--- src/components/TabBarButton.tsx
+++ src/components/TabBarButton.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View, Text, Pressable, StyleSheet } from 'react-native'
 import{ icon } from "../../constants/icon"
+import Animated, { interpolate, useAnimatedStyle, useSharedValue, withSpring } from 'react-native-reanimated';
 
  const TabBarButton = ({onPress,onLongPress,isFocused,routeName,color,label}:{
     onPress: Function;
@@ -10,18 +11,50 @@ import{ icon } from "../../constants/icon"
     color: string;
     label:string;
 }) => {
+  const scale = useSharedValue(0);
+
+  useEffect(() => {
+    scale.value = withSpring(
+      typeof isFocused === 'boolean' ? (isFocused ? 1 : 0) : isFocused, 
+      {duration: 350}
+  );
+  }, [scale, isFocused])
+
+  const animatedIconStyle  = useAnimatedStyle(() => {
+    const scaleValue = interpolate(scale.value, [ 0 , 1 ],[1, 1.2])
+
+    const top = interpolate(scale.value, [ 0 , 1 ], [ 0 , 9 ])
+
+    return {
+      transform: [{
+        scale: scaleValue
+      }],
+      top
+    }
+  })
+
+  const animatedTextStyle = useAnimatedStyle(() => {
+    const opacity = interpolate(scale.value, [ 0, 1 ], [ 1 , 0 ])
+
+    return {
+      opacity
+    }
+  });
+
   return (
         <Pressable
             onPress={onPress}
             onLongPress={onLongPress}
             style={styles.tabbarItem}
           >
-            {icon[routeName]({
-                color: isFocused ? '#673ab7' : '#222' ,
-            })}
-            <Text style={{ color: isFocused ? '#673ab7' : '#222' }}>
+            <Animated.View style={animatedIconStyle} >
+              {icon[routeName]({
+                  color: isFocused ? '#673ab7' : '#222' ,
+              })}
+            </Animated.View>
+            <Animated.Text style={[{ color: isFocused ? '#673ab7' : '#222', fontSize: 12 }, animatedTextStyle]}>
               {label}
-            </Text>
+            </Animated.Text>
         </Pressable>
   )
 }
