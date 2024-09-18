24a82b63bd51037e9ef6c12238b21513ff6f6e22
Arthur Borges <borgesarthur.dev@gmail.com>
2024-09-17 21:24:19 -0300

    Started archives screen

--- src/screens/Arquivos.tsx
+++ src/screens/Arquivos.tsx
@@ -1,5 +1,4 @@
-// import { Input } from "@components/Input";
-import { Center, HStack, Input, Text, VStack } from "@gluestack-ui/themed";
+import { Box, Card, Center, HStack, Input, Text, VStack } from "@gluestack-ui/themed";
 import { Feather } from "@expo/vector-icons";
 import { Pressable } from "react-native";
 
@@ -9,21 +8,31 @@ import { Pressable } from "react-native";
 
 export const Arquivos = ( ) => {
     return (
-        <VStack flex={1} justifyContent="center" bg="$gray600" >
-             {/* <Input placeholder="Buscar arquivo" autoCorrect={false} /> */}
-             <Input justifyContent="space-between" alignItems="center" mx="5%" px="$2" borderRadius="$lg" h="5%" >
-             <Text color="$gray400" fontFamily="$heading" >Buscar arquivo</Text>
-                <HStack mx="$2" alignItems="center" >
-                    <Pressable onPress={() => console.log("piru1")}>
-                          <Feather name="search" size={23} />
-                    </Pressable>
-                    <Text  fontSize="$3xl" color="$gray400" mx="$2.5" >I</Text>
-                    <Pressable onPress={() => console.log("piru2")}>
-                          <Feather name="sliders" size={23} />
-                    </Pressable>
+        <VStack flex={1}  bg="$gray600" >
+            <VStack pt="$14" mb="$10" pb="$5" bg="$gray700" borderRadius="$2xl"
+                    shadowColor="$gray300"
+                    shadowOffset={{ width: 0, height: 3 }}
+                    shadowRadius={6}
+                    shadowOpacity={0.2} 
+            >
 
-                </HStack>
-             </Input>
+                <Input  bg="$gray600" justifyContent="space-between" alignItems="center" mx="5%" px="$2" borderRadius="$lg"  >
+                <Text color="$gray400" fontFamily="$heading" >Buscar arquivo</Text>
+                    <HStack mx="$2" alignItems="center" >
+                        <Pressable onPress={() => console.log("piru1")}>
+                            <Feather name="search" size={23} />
+                        </Pressable>
+                        <Text  fontSize="$3xl" color="$gray400" mx="$2.5" >I</Text>
+                        <Pressable onPress={() => console.log("piru2")}>
+                            <Feather name="sliders" size={23} />
+                        </Pressable>
+
+                    </HStack>
+                </Input>
+            </VStack>
+            <VStack flex={1} mx="5%">
+                <Card />
+            </VStack>
         </VStack>
     )
 }
