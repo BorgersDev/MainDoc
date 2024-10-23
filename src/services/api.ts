01e8b4f3fa04904acb1b40ad94e466b406f0fd96
Arthur Borges <borgesarthur.dev@gmail.com>
2024-10-23 11:13:49 -0300

    AppErrors and treatings done

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,16 @@
 import axios from "axios";
+import { AppError } from "@utils/AppError";
 
-export const api = axios.create({
+const api = axios.create({
     baseURL: 'https://maindoc.com.br'
 })
+
+api.interceptors.response.use((response) => response, error => {
+    if(error.response && error.response.data){
+        return Promise.reject(new AppError(error.response.data.mensagem));
+    } else {
+        return Promise.reject( new AppError('Erro no servidor. Tente novamente mais tarde.'))
+    }
+})
+
+export { api }
