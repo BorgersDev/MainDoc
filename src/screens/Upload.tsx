834599cf536f4d0d3a07553da6953e3ca557b6a9
Arthur Borges <borgesarthur.dev@gmail.com>
2025-03-18 07:16:28 -0300

    Upload screen

--- src/screens/Upload.tsx
+++ src/screens/Upload.tsx
@@ -7,6 +7,7 @@ import {Feather} from "@expo/vector-icons"
 import { TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { AppNavigationRoutesProps } from "@routes/app.routes";
+import { Box } from "@/components/ui/box";
 
 
 
@@ -22,9 +23,34 @@ export const Upload = ( ) => {
                         </HStack>
                     </TouchableOpacity>
              </HStack>
-                <Center className="flex-1">
-                    <Text className="font-heading text-lg">Upload Screen</Text>
-                </Center>
+             <VStack className="flex-1 items-center gap-2">
+                <Box
+                    className="mt-[7%] w-[90%] h-[40%] bg-gray-200 rounded-2xl"
+                    style={{
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: -1 }, // Moves shadow upwards
+                    shadowOpacity: 0.3,
+                    shadowRadius: 10,
+                    }}
+                >
+                    <TouchableOpacity className="flex-1 m-12">
+                        <Box className=" justify-center items-center flex-1 border-2 border-dashed border-blue-950  rounded-2xl "  >
+                            <Feather name="upload" size={30} color={"#00419d"}/>
+                            <Text className="font-heading color-blue-950">Adicionar arquivo</Text>
+                        </Box>
+                    </TouchableOpacity>
+                </Box>
+
+                <Box
+                    className="mt-[7%] w-[90%] h-[45%] bg-gray-200 rounded-2xl"
+                    style={{
+                    shadowColor: "#000",
+                    shadowOffset: { width: 0, height: 2 }, // Moves shadow downwards
+                    shadowOpacity: 0.3,
+                    shadowRadius: 9,
+                    }}
+                />
+                </VStack>
             </VStack>
         </SafeAreaView>
     );
